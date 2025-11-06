4629729ea1c5d413d109d2f8535b331fd6337f16
hermanfugl <hefug3854@met.no>
2025-11-06 18:42:45 +0100

    fix input/output paths

--- 02_build_ppe.py
+++ 02_build_ppe.py
@@ -2,15 +2,19 @@ from pathlib import Path
 from tinkertool.scripts.create_ppe.config import BuildPPEConfig
 from tinkertool.scripts.create_ppe.create_ppe import build_ppe
 
+# Perform commands from the script directory pov
+script_dir = Path(__file__).parent
+
+
 buildppe_config = BuildPPEConfig(
-  simulation_setup_path = Path('/cluster/projects/nn9560k/johannef/NorESM_workdir/cases-test_tinkertool4coupled/run_scripts/input_files/simulation_setup.ini').resolve(),
-  build_base_only       = False,
+  simulation_setup_path   = script_dir.joinpath('input_files', 'simulation_setup.ini').resolve(),
+  build_base_only         = False,
   clone_only_during_build = False,
-  keepexe               = False,
-  overwrite             = True,
-  verbose               = 2,
-  log_dir               = Path(f'/cluster/projects/nn9560k/johannef/NorESM_workdir/cases-test_tinkertool4coupled/run_scripts/output_files/logs').resolve(),
-  log_mode              = 'w'
+  keepexe                 = False,
+  overwrite               = True,
+  verbose                 = 2,
+  log_dir                 = script_dir.joinpath('output_files', 'logs').resolve(),
+  log_mode                = 'w'
 )
 
 build_ppe(buildppe_config)
