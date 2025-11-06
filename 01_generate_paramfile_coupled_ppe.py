12749de9e75e85ca745cb5cf486b1b5a7e6241a4
hermanfugl <hefug3854@met.no>
2025-11-06 18:54:02 +0100

    fix input/output paths

--- 01_generate_paramfile_coupled_ppe.py
+++ 01_generate_paramfile_coupled_ppe.py
@@ -14,7 +14,7 @@ script_dir = Path(__file__).parent
 # --- define parameter file configuration ---
 parm_conf = ParameterFileConfig(
     param_ranges_inpath=script_dir.joinpath('input_files', 'NorESM3_tuningparameterinfo_PPE.ini').resolve(),
-    param_sample_outpath=script_dir.joinpath('input_files', 'NorESM3_tuningparameterinfo_PPE_test.nc').resolve(),
+    param_sample_outpath=script_dir.joinpath('input_files', 'NorESM3_tuningparameterinfo_PPE.nc').resolve(),
     chem_mech_file=None,
     ctsm_default_param_file=Path('/cluster/shared/noresm/inputdata/lnd/clm2/paramdata/ctsm60_params.250923_v26j.nc'),
     fates_default_param_file=Path('/cluster/shared/noresm/inputdata/share_kjetil/Test_param_files/fates_params_sci.1.85.1_api.40.0.0_14pft_nor_sci1_api1TEST_c251105.nc'),
@@ -38,7 +38,7 @@ print("\n", ds_string)
 
 visualize_paramfile(
     parm_conf.param_sample_outpath.with_suffix('.raw.nc'),
-    save_path=script_dir.joinpath('output_files', 'figures', 'NorESM3_tuningparameterinfo_PPE_test_pairplot.png').resolve(),
+    save_path=script_dir.joinpath('output_files', 'figures', 'NorESM3_tuningparameterinfo_PPE_pairplot.png').resolve(),
 )
 
 # %%
